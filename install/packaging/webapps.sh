10e74dee2f4dcec73d8f14f7778ab686fd808a22
Edward Coombs <edward.coombs@student.adelaide.edu.au>
2025-12-04 20:57:38 +1030

    Adding web apps for teams, outlook, Google calendar

--- install/packaging/webapps.sh
+++ install/packaging/webapps.sh
@@ -1,3 +1,6 @@
 omarchy-webapp-install "WhatsApp" https://web.whatsapp.com/ WhatsApp.png
 omarchy-webapp-install "GitHub" https://github.com/ GitHub.png
 omarchy-webapp-install "Discord" https://discord.com/channels/@me Discord.png
+omarchy-webapp-install "Teams" https://teams.microsoft.com/v2/ Teams.png
+omarchy-webapp-install "Outlook" https://outlook.office.com/mail/ Outlook.png
+omarchy-webapp-install "Calendar" https://calendar.google.com/calendar/u/0/r Calendar.png
